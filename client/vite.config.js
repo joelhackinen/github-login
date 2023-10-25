9a8ce5f470e8d82e908b54f583d3801e0fcc2c9b
joel_laptop <joel.hakkinen@gmail.com>
2023-10-25 11:13:58 +0300

    fix vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) =>
-  command === "server"
+  command === "serve"
     ? {
         plugins: [react()],
         server: {
